680e62eaca3221dc442ea307f23e08510f384a8f
ffenland <ffenland@gmail.com>
2023-07-19 09:54:43 +0900

    poetry down

--- build.sh
+++ build.sh
@@ -2,7 +2,19 @@
 # exit on error
 set -o errexit
 
-poetry install
+echo "Installing the latest version of poetry..."
+
+pip install --upgrade pip
+
+pip install poetry==1.2.0
+
+rm poetry.lock
+
+poetry lock
+
+python -m poetry install
+
+pip install --force-reinstall -U setuptools
 
 python manage.py collectstatic --no-input
 python manage.py migrate
